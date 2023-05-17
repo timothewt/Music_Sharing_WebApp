f69ae683c0ca90117f1f4e02ecf9c3363cf127eb
Timothe Watteau <timothe.wt@gmail.com>
2023-05-17 02:22:19 +0200

    Filled database with spotify dataset including artists, albums and their songs. Almost 10,000 songs are currently in the db.

---
+++ backend/db_data/db.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from users.models import User
+from songs.models import Album, AlbumTag, Song, SongTag
+import random
+import string
+
+
+def insert_artists():  # execute first
+	artists = pd.read_csv("db_data/artists.csv")
+	for i in range(len(artists)):
+		User.objects.create_user(artists.loc[i,"name"], password=''.join(random.choices(string.ascii_uppercase + string.digits, k=16)))
+
+
+def insert_albums():  # execute second
+	albums = pd.read_csv("db_data/albums.csv")
+	for i in range(len(albums)):
+		artist = User.objects.filter(username=albums.loc[i,"artist"]).first()
+		album = Album(name=albums.loc[i,"name"], release_year=albums.loc[i,"year"], artist=artist, description='', cover_link=albums.loc[i,"cover_link"])
+		album.save()
+		try:
+			for tag in albums.loc[i,"tags"].split(', '):
+				album_tag = AlbumTag(tag=tag, album=album)
+				album_tag.save()
+		except AttributeError:
+			print(f"Unable to insert tag : {albums.loc[i,'tags']}, from album {album}")
+
+
+def insert_songs():  # execute third
+	songs = pd.read_csv("db_data/songs.csv")
+	for i in range(len(songs)):
+		artist = User.objects.filter(username=songs.loc[i,"artist"]).first()
+		if artist is None:
+			continue
+
+		album = Album.objects.filter(artist=artist, name=songs.loc[i,"album"]).first()
+		if album is None:
+			continue
+
+		song = Song(name=songs.loc[i,"name"], release_year=songs.loc[i,"year"], artist=artist, album=album, recording_link=songs.loc[i,"spotify_preview_url"], duration_ms=songs.loc[i,"duration_ms"])
+		song.save()
+
+		try:
+			for tag in songs.loc[i,"tags"].split(', '):
+				song_tag = SongTag(tag=tag, song=song)
+				song_tag.save()
+		except AttributeError:
+			print(f"Unable to insert tag : {songs.loc[i,'tags']}, from song {song}")
--- backend/songs/models.py
+++ backend/songs/models.py
@@ -25,32 +25,15 @@ class Album(models.Model):
 
 class Song(models.Model):
 
-	date_uploaded = models.DateTimeField(auto_now_add=True, editable=False)
 	release_year = models.IntegerField()
 	name = models.CharField(max_length=255)
 	artist = models.ForeignKey(User, on_delete=models.CASCADE)
 	album = models.ForeignKey(Album, on_delete=models.CASCADE)
 	recording_link = models.CharField(max_length=255)
-	track_number = models.IntegerField()
 	duration_ms = models.IntegerField(blank=True, default=0)
-	explicit = models.BooleanField(default=False)
-	# danceability = models.DecimalField(max_digits=4, decimal_places=3)
-	# energy = models.DecimalField(max_digits=4, decimal_places=3)
-	# loudness = models.DecimalField(max_digits=5, decimal_places=3)
-	# speechiness = models.DecimalField(max_digits=5, decimal_places=4)
-	# acousticness = models.DecimalField(max_digits=5, decimal_places=4)
 
 	def __str__(self):
 		return f"{self.name} by {self.artist} from {self.album.name}, {self.release_year}"
-
-
-class SongTag(models.Model):
-
-	tag = models.CharField(max_length=255)
-	song = models.ForeignKey(Song, on_delete=models.CASCADE)
-
-	def __str__(self):
-		return self.tag
 
 
 class AlbumTag(models.Model):
@@ -59,7 +42,16 @@ class AlbumTag(models.Model):
 	album = models.ForeignKey(Album, on_delete=models.CASCADE)
 
 	def __str__(self):
-		return self.tag
+		return f"{self.tag} : {self.album}"
+
+
+class SongTag(models.Model):
+
+	tag = models.CharField(max_length=255)
+	song = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return f"{self.tag} : {self.song}"
 
 
 class Playlist(models.Model):
