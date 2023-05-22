e28a7faa14a4d8e2b43e1b01e7c30e8f29b18d68
HugoM25 <hddm25@gmail.com>
2023-05-22 18:03:25 +0200

    Feat: Implemented basis of song page

--- frontend/src/app/pages/song-page/song-page.component.ts
+++ frontend/src/app/pages/song-page/song-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Song } from 'src/app/models/song';
 
 @Component({
   selector: 'app-song-page',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./song-page.component.scss']
 })
 export class SongPageComponent {
+  @Input() song!: Song;
+
+  constructor() { }
 
 }
