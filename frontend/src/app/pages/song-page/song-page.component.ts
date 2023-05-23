7d5e9f2dfeebafa984ef681cfdf08bf905465648
HugoM25 <hddm25@gmail.com>
2023-05-23 16:49:41 +0200

    Feat: Added dynamic song page generation

--- frontend/src/app/pages/song-page/song-page.component.ts
+++ frontend/src/app/pages/song-page/song-page.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { Song } from 'src/app/models/song';
+import { Album } from 'src/app/models/album';
+import { User } from 'src/app/models/user';
+
+import { ActivatedRoute } from '@angular/router';
+import { APIService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-song-page',
@@ -7,8 +12,23 @@ import { Song } from 'src/app/models/song';
   styleUrls: ['./song-page.component.scss']
 })
 export class SongPageComponent {
-  @Input() song!: Song;
 
-  constructor() { }
+  public song: Song = new Song();
+
+  constructor(private _Activatedroute:ActivatedRoute, private apiService: APIService) { 
+    console.log("SongPageComponent constructor");
+    this._Activatedroute.paramMap.subscribe(params => {
+      //Get the song id from the url
+      let songId : number  = Number(params.get('id'));
+
+      //Load the song infos from the backend
+      this.apiService.getSongById(songId).subscribe(
+        (response: any) => {
+          console.log(response);
+          this.song.deserialize(response);
+        }
+      );
+    });
+  }
 
 }
