914a2e79b1d2449fef21d0876401e254cb9608a5
HugoM25 <hddm25@gmail.com>
2023-05-29 20:00:21 +0200

    Feat: Can now click on album to load into player

--- frontend/src/app/utility/components/album/album.component.ts
+++ frontend/src/app/utility/components/album/album.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input} from '@angular/core';
 import { Album } from 'src/app/models/album';
+import { Song } from 'src/app/models/song';
+import { APIService } from 'src/app/services/api.service';
+import { SharedQueueService } from 'src/app/services/shared-queue.service';
 
 @Component({
   selector: 'app-album',
@@ -9,7 +12,52 @@ import { Album } from 'src/app/models/album';
 export class AlbumComponent {
   
     @Input() album!: Album;
+
+    songs: Song[] = [];
   
-    constructor() { 
+    constructor(private sharedQueueService: SharedQueueService, private apiService: APIService,) {}
+
+    public addAlbumToQueue(){
+      //Load the songs of the album from the backend if not already loaded
+      if (this.songs.length == 0) {
+        this.loadSongs().then(() => {
+          //Add the songs to the queue
+          this.addSongsToQueue();
+
+        });
+      }
+      else {
+        //Add the songs to the queue
+        this.addSongsToQueue();
+      }
+
     }
+
+    public async loadSongs() : Promise<void>{
+      return new Promise<void>((resolve, reject) => {
+        this.apiService.getSongs({albumId: this.album.id}).subscribe(
+          (response: any) => {
+            this.songs = [];
+            for(let i = 0; i < response.length; i++) {
+              let song = new Song().deserialize(response[i]);
+              this.songs.push(song);
+            }
+            resolve();
+          }
+        );
+      }
+      );
+    }
+
+    public addSongsToQueue(songIndex?: number): void {
+      let queue = this.sharedQueueService.getQueue();
+      queue.setSongs(this.songs);
+      if (songIndex) {
+        queue.setCurrentSongIndex(songIndex);
+      }
+  
+      this.sharedQueueService.setDoReloadPlayer(true);
+
+    }
+
 }
