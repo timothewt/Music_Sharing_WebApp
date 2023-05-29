f81df3054e41a17b1efeaa71ec3e633c3f03b970
HugoM25 <hddm25@gmail.com>
2023-05-29 20:14:08 +0200

    Fix: Router issues fixed and album style updated

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page/home-page.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', component: HomePageComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomePageComponent},
   { path: 'artist', loadChildren: () => import('./artist-page/artist-page.module').then(m => m.ArtistPageModule)},
   { path: 'song', loadChildren: () => import('./song-page/song-page.module').then(m => m.SongPageModule) },
   { path: 'album', loadChildren: () => import('./album-page/album-page.module').then(m => m.AlbumPageModule) },
