0c4b9cf0a01c2314cf89d1ab8f9cf2d3da5d078e
HugoM25 <hddm25@gmail.com>
2023-05-31 17:23:55 +0200

    Fix: Changed style to optimize pages

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ArtistPageRoutingModule } from './artist-page/artist-page-routing.modul
 import { AlbumPageRoutingModule } from './album-page/album-page-routing.module';
 import { SongPageRoutingModule } from './song-page/song-page-routing.module';
 import { PlayerModule } from './player/player.module';
+import { HomePageModule } from './home-page/home-page.module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,6 +30,7 @@ import { PlayerModule } from './player/player.module';
     SongPageRoutingModule,
     ArtistPageRoutingModule,
     PlayerModule,
+    HomePageModule,
   ],
   providers: [APIService,],
   bootstrap: [AppComponent]
--- frontend/src/app/home-page/home-page.module.ts
+++ frontend/src/app/home-page/home-page.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page/home-page.component';
-
-
+import { UtilityModule } from '../utility/utility.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     HomePageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    UtilityModule,
   ],
   exports: [
     HomePageComponent
